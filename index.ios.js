11a99417d36fcef8d4ff7cec227d53452b78011d
Chuong Le <chuong@poeticsystems.com>
2017-07-25 18:51:50 -0500

    sample js call to native.

---
+++ index.ios.js
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  NativeModules
+} from 'react-native';
+
+const { MakeCallNative } = NativeModules;
+
+export default class makecall extends Component {
+  call() {
+    return MakeCallNative.callMe("(713) 391-6596"); 
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+        <Button
+          onPress={this.call}
+          title="Call Me Objective C"
+          color="#841584"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('makecall', () => makecall);
